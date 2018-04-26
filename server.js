6920f2c25ab10ad234e18e72ce508099bbd506b4
Derrick Persson <derrickpersson@gmail.com>
2018-04-26 06:29:59 -0700

    Set up Knex

---
+++ server.js
@@ -0,0 +1,19 @@
+// Routes:
+
+/* 
+
+/web/[URL]
+  -> MVP: Save website at the URL as PDF
+  -> PII: Save website at the URL as PDF & send via email to specified email
+  -> PIII: Build chrome extension; button to click to send to PDF
+
+/signup
+  -> Sign up a new account
+
+/profile
+  -> Set:
+    - Kindle Email
+    - My email?
+    - Reset my password?
+
+*/

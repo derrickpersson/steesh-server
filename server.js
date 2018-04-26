c45e57bfa0909f139c731a95a910187d1268dc23
Derrick Persson <derrickpersson@gmail.com>
2018-04-26 07:06:12 -0700

    Added mock routes

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
-
 // Routes:
 
 /* 
@@ -24,3 +22,17 @@ app.listen(8080, () => console.log('Example app listening on port 8080!'))
     - Reset my password?
 
 */
+
+app.get('/web/:url', (request, response) => {
+  response.send("Getting website as PDF now...");
+})
+
+app.get('/signup', (request, response) => {
+  response.send("Sign up here!");
+})
+
+app.get('/profile', (request, response) => {
+  response.send("View your information here.");
+})
+
+app.listen(8080, () => console.log('Example app listening on port 8080!'))

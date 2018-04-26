e71ee9f6c7b898ec64493262f1ab5988f958d6c1
Derrick Persson <derrickpersson@gmail.com>
2018-04-26 06:57:38 -0700

    Added initial migrations for users and articles table

---
+++ db/migrations/20180426064607_createUserAndArticlesTables.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id');
+    table.string('email');
+    table.string('password');
+  }).then(function(){
+    return knex.schema.createTable('articles', function(table){
+      table.increments('id');
+      table.string('URL');
+      table.integer('user_id').references('users.id');
+    });
+  });
+  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('articles').then(function(){
+    return knex.schema.dropTable('users');
+  });
+};
--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,10 @@ module.exports = {
     connection: {
       host     : process.env.DB_HOST,
       user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
+      //password : process.env.DB_PASS,
       database : process.env.DB_NAME,
       port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      //ssl      : process.env.DB_SSL
     },
     migrations: {
       directory: './db/migrations',

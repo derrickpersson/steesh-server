59295b23ba545e627c6373379ae4283a0fcbff23
Derrick Persson <derrickpersson@gmail.com>
2018-04-27 07:09:58 -0700

    Included ejs and added index page

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
 const express = require('express')
+const bodyParser = require("body-parser");
+
+
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.get('/', (req, res) => res.render('index'))
 
 // Routes:
 
-/* 
+// /web/[URL]
+//   -> MVP: Save website at the URL as PDF
+//   -> PII: Save website at the URL as PDF & send via email to specified email
+//   -> PIII: Build chrome extension; button to click to send to PDF
 
-/web/[URL]
-  -> MVP: Save website at the URL as PDF
-  -> PII: Save website at the URL as PDF & send via email to specified email
-  -> PIII: Build chrome extension; button to click to send to PDF
-
-/signup
-  -> Sign up a new account
-
-/profile
-  -> Set:
-    - Kindle Email
-    - My email?
-    - Reset my password?
-
-*/
-
-app.get('/web/:url', (request, response) => {
+app.post('/getPDF', (request, response) => {
+  console.log(request.body.URL);
+  request.body.URL
   response.send("Getting website as PDF now...");
 })
+
+// /signup
+//   -> Sign up a new account
 
 app.get('/signup', (request, response) => {
   response.send("Sign up here!");
 })
+
+// /profile
+//   -> Set:
+//     - Kindle Email
+//     - My email?
+//     - Reset my password?
 
 app.get('/profile', (request, response) => {
   response.send("View your information here.");

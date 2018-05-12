b7d589fd6d0a3eece4b261b09a31976be0c7fd7b
Derrick Persson <derrickpersson@gmail.com>
2018-05-12 10:29:39 -0700

    Mailgun set up - waiting on DNS verification to test

---
+++ mailgun.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+let requestURL = `https://api:${process.env.MG_KEY}@api.mailgun.net/v3/samples.mailgun.org/log`
+
+const path = require('path');
+const DOMAIN = 'mail.steesh.ca';
+const api_key = process.env.MG_KEY;
+const mailgun = require('mailgun-js')({ apiKey: api_key, domain: DOMAIN });
+
+const filepath = path.join(__dirname, 'medium.pdf');
+
+let data = {
+  from: 'Derrick Persson <derrickpersson@gmail.com>',
+  to: 'derrickpersson@kindle.com',
+  subject: 'Convert',
+  text: 'Sending article to Kindle',
+  attachment: filepath
+};
+
+mailgun.messages().send(data, function (error, body) {
+  console.log(body);
+});

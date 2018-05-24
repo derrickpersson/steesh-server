f57eaa5d5799443fee3b4d6505f71d6137d41979
Derrick Persson <derrickpersson@gmail.com>
2018-05-23 19:22:09 -0700

    Successfully parsed article title

--- scripts/parseTitle.js
+++ scripts/parseTitle.js
@@ -1,42 +1,25 @@
-function parseTitle(title){
-  let titleArray = title.split("/");
-  let lastElement = titleArray[(titleArray.length - 1)];
-  let parsedTitle = lastElement.split("-").join(" ");
-  return parsedTitle;
-}
+'use strict';
 
-function parseURL(url){
-    var a = document.createElement('a');
-    a.href = url;
-    return {
-        source: url,
-        protocol: a.protocol.replace(':',''),
-        host: a.hostname,
-        port: a.port,
-        query: a.search,
-        params: (function(){
-            var ret = {},
-            seg = a.search.replace(/^\?/,'').split('&'),
-            len = seg.length, i = 0, s;
-            for (;i<len;i++) {
-                if (!seg[i]) { continue; }
-                s = seg[i].split('=');
-                ret[s[0]] = s[1];
-            }
-            return ret;
-        })(),
-        file: (a.pathname.match(/\/([^\/?#]+)$/i) || [,''])[1],
-        hash: a.hash.replace('#',''),
-        path: a.pathname.replace(/^([^\/])/,'/$1'),
-        relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [,''])[1],
-        segments: a.pathname.replace(/^\//,'').split('/')
-    };
+const URL = require('url-parse');
+// const Regex = require("regex");
+
+const articleURL = new URL("https://betterhumans.coach.me/get-happy-like-the-danes-a-practical-guide-to-more-hygge-in-your-life-10e1f0b5363d");
+
+// const findHTMLClause = new Regex(/\./)
+
+function parseTitle(url){
+  let articleURL = new URL(url);
+  let pathname = articleURL.pathname;
+  let pathnameArray = pathname.split("/");
+  let titleElement = (pathnameArray[(pathnameArray.length - 1)].length === 0) ? pathnameArray[(pathnameArray.length - 2)] : pathnameArray[(pathnameArray.length - 1)];
+  titleElement = titleElement.replace(/\..*/, "");
+  titleElement = titleElement.split("-").join(" ");
+  return titleElement;
 }
 
 console.log(parseTitle("https://betterhumans.coach.me/get-happy-like-the-danes-a-practical-guide-to-more-hygge-in-your-life-10e1f0b5363d"));
 console.log(parseTitle("https://www.nytimes.com/interactive/2018/05/23/us/politics/trump-mueller-russia.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=photo-spot-region&region=top-news&WT.nav=top-news"));
 console.log(parseTitle("https://www.theglobeandmail.com/world/article-canadian-cannabis-industry-gives-boost-to-southern-african-countries/"));
-console.log(parseURL("https://www.theglobeandmail.com/world/article-canadian-cannabis-industry-gives-boost-to-southern-african-countries/"));
 
 
 

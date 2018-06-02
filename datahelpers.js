030bc802eaa83834d4e3bdce2a153b2dd518a420
Derrick Persson <derrickpersson@gmail.com>
2018-06-02 12:11:13 -0700

    Registering users working

--- datahelpers.js
+++ datahelpers.js
@@ -1,23 +1,14 @@
-const bcrypt = require('bcrypt');
-let salt = bcrypt.genSaltSync(11);
-
 module.exports = function makeDataHelpers(db) {
   return {
     insertUser: function(userData){
       let {firstName, lastName, email, kindleEmail, password } = userData;
-      bcrypt.hash(password, salt, null, function(err, hash) {
-        if(err) return err;
-        let password = hash;
-      }).then(function(err){
-        return db('users').insert({
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          kindleEmail: kindleEmail,
-          password: password
-        });
-      })
-
+      return db('users').insert({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        kindleEmail: kindleEmail,
+        password: password
+      });
     },
     getUserByEmail: function(email) {
       return db.select("*").from('users').where('email', email);

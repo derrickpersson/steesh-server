3be4a56dccda3552c14042f9f4fee087c82bb9a0
Derrick Persson <derrickpersson@gmail.com>
2018-06-13 20:51:38 -0700

    One click working

--- chromeExtension/background.js
+++ chromeExtension/background.js
@@ -3,8 +3,14 @@
 chrome.runtime.onInstalled.addListener(function() {
 
   chrome.storage.sync.get(['signed_in'], function(data) {
+    console.log("Signed In? ", data.signed_in);
     if (data.signed_in) {
-      chrome.browserAction.setPopup({popup: 'popup.html'});
+      chrome.browserAction.onClicked.addListener(function(tab) { 
+        createDataPackageToSend(tab.url).then(function(data){
+          sendData(data);
+          alert('Data Sent:' + " " + data.userID + ", " + data.URL);
+        })
+      });
     } else {
       chrome.tabs.create({url: 'sign_up.html'});
     }
@@ -15,7 +21,12 @@ chrome.runtime.onInstalled.addListener(function() {
   console.log("areaName: ", areaName);
   chrome.storage.sync.get(['signed_in'], function(data) {
       if (data.signed_in) {
-        chrome.browserAction.setPopup({popup: 'popup.html'});
+        chrome.browserAction.onClicked.addListener(function(tab) { 
+          createDataPackageToSend(tab.url).then(function(data){
+            sendData(data);
+            alert('Data Sent:' + " " + data.userID + ", " + data.URL);
+          })
+        });
       } else {
         chrome.browserAction.setPopup({popup: 'sign_up.html'});
       }
@@ -26,6 +37,31 @@ chrome.runtime.onInstalled.addListener(function() {
 
 });
 
+function createDataPackageToSend(URL){
+  return new Promise(function(resolve, reject){
+    chrome.storage.sync.get(null, function(storage){
+      let data = {
+        URL: URL,
+        userID: storage.userID
+      };
+      resolve(data);
+    })
+  })
+}
+
+function sendData(data){
+  var xhr = new XMLHttpRequest();
+  // xhr.withCredentials = true;
+  xhr.open("POST", "http://127.0.0.1:8080/getPDF", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      // JSON.parse does not evaluate the attacker's scripts.
+      // var resp = JSON.parse(xhr.responseText);
+    }
+  }
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(data));
+}
 
 
 
